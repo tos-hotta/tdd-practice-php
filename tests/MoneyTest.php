dae68ce8505e4c8d3fab55d1dcdee47be9fa7fd1
Toshiko Sato <toshiko@bltinc.co.jp>
2021-05-26 12:53:43 +0900

    ２章　明白な実装

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -9,7 +9,10 @@ class MoneyTest extends TestCase
     public function testMultiplication(): void
     {
         $five = new Doller(5);
-        $five->times(2);
-        $this->assertEquals(10, $five->amount);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
     }
 }
